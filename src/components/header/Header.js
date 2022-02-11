07c87d031657481c601e6d27fecf7136f682174a
Vovukanp31 <vpapeta31@gmail.com>
2022-02-11 03:30:57 +0200

    updated webLogo/link to homePage

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,28 +2,31 @@ import React from 'react';
 
 import {Dropdown, DropdownButton} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 import css from './header.module.css';
 import UserInfo from "../userInfo/UserInfo";
 
 
-
-
 const Header = () => {
+
+    const homepage = useNavigate();
 
     return (
         <div className={css.header}>
 
             <div className={css.actionPart}>
-                <img src="https://i.postimg.cc/3xFLWyDr/webLogo.png" alt=""/>
+                <img src="https://i.postimg.cc/3xFLWyDr/webLogo.png" alt="" onClick={() => {
+                    homepage('/');
+                }}/>
+
                 <DropdownButton id="dropdown-basic-button" title="Genres" className={css.headerBtn}>
                     <Dropdown.Item> <Link to={'/vova'}>vova</Link></Dropdown.Item>
                     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
                     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
                 </DropdownButton>
-            </div>
 
+            </div>
             <UserInfo/>
         </div>
     );
